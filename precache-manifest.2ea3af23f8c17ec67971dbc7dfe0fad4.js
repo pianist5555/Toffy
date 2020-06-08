fcbaf2dcb7e3a12bf5ac4f9395d43e98f0e98114
Seono <pianist5555@gmail.com>
2020-06-09 04:46:02 +0900

    Updates

---
+++ precache-manifest.2ea3af23f8c17ec67971dbc7dfe0fad4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ae3f4834c0fddffd61ad2930f17239",
+    "url": "/toffy/index.html"
+  },
+  {
+    "revision": "53d05db6b3854287d70b",
+    "url": "/toffy/static/css/main.7ea5fe64.chunk.css"
+  },
+  {
+    "revision": "68a0c9f8716f3919d475",
+    "url": "/toffy/static/js/2.8d202ed3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/toffy/static/js/2.8d202ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53d05db6b3854287d70b",
+    "url": "/toffy/static/js/main.8e660b34.chunk.js"
+  },
+  {
+    "revision": "e76721d2e061f802aed4",
+    "url": "/toffy/static/js/runtime-main.664e5fa7.js"
+  },
+  {
+    "revision": "9e1b6abbacb71d57114c14dd4fd54b64",
+    "url": "/toffy/static/media/A_back.9e1b6abb.png"
+  }
+]);
